ba849d2cb5d145bfddb99fe519b4f0c4d859821d
장형원 <janghyung-won@janghyeong-won-ui-MacBookAir.local>
2022-05-24 21:27:50 +0900

    make TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1 class="text-center mb-40">Todo App</h1>
+      <input type="text" class="form-control mb-40" v-model="userInput" @keyup.enter="addNewTodos">
+      <div class="list-group">
+        <button class="list-group-item text-left" @click="toggleTodoState(todo)" v-for="(todo, idx) in activeTodoList " :key="idx">{{todo.label}}</button>
+      </div>
+      <div class="text-right">
+        <button class="btn btn-sm" @click="changeCurrentState('active')">Todo</button>
+        <button class="btn btn-sm" @click="changeCurrentState('done')">Done</button>
+        <button class="btn btn-sm" @click="changeCurrentState('all')">Entire</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      userInput: "",
+      todoList: [],
+      currentState: 'active',
+    }
+  },
+  computed: {
+    activeTodoList() {
+      return this.todoList.filter(todo => this.currentState === 'all' || todo.state === this.currentState)
+    },
+  },
+  methods: {
+    changeCurrentState(state) {
+      this.currentState = state
+    },
+    addNewTodos() {
+      this.todoList.push({
+        label: this.userInput,
+        state: 'active',
+      })
+      this.userInput = ''
+    },
+    toggleTodoState(todo) {
+      todo.state = todo.state === 'active' ? 'done' : 'active'
+    }
+  },
   components: {
-    HelloWorld
-  }
+    // HelloWorld
+  },
 }
 </script>
 
